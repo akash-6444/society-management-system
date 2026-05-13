20e891679de50b5cc42c73f1823bbd7c82594090
akash <tembekarakash@gmail.com>
2026-05-13 23:34:16 +0530

    fixed mail again

--- server.js
+++ server.js
@@ -44,13 +44,32 @@ db.connect((err) => {
 
 // EMAIL TRANSPORTER
 const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
+
+    service: 'gmail',
+
     auth: {
+
         user: process.env.EMAIL_USER,
+
         pass: process.env.EMAIL_PASS
+
     }
+
+});
+
+transporter.verify((error, success) => {
+
+    if (error) {
+
+        console.log("MAIL ERROR:");
+        console.log(error);
+
+    } else {
+
+        console.log("MAIL SERVER READY");
+
+    }
+
 });
 
 // LOGIN PAGE
