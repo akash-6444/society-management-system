ef118586a76b7510304d0c8d6ccd8e0325a70558
akash <tembekarakash@gmail.com>
2026-05-13 23:04:33 +0530

    mail

--- database/db.js
+++ database/db.js
@@ -1,14 +1,11 @@
 const mysql = require('mysql2');
 
-const pool = mysql.createPool({
+const connection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: process.env.DB_PORT,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
+    port: process.env.DB_PORT
 });
 
-module.exports = pool;
+module.exports = connection;
